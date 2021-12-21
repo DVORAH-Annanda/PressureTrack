2c3bbe148f433fab67ab541df612ec8af8c64dae
Annanda <Annanda@dvorahsolutions.co>
2021-12-21 20:28:59 +0200

    Wheeldiagram - conditional styling

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -10,29 +10,55 @@ const Wheel = (props) => {
     console.log(`PROPS WHEEL ${JSON.stringify(props)}`);
   }
 
+  renderWheelStyle = () => {
+    if(props.children.pressureValue > props.children.maxPressureValue){
+      return (
+        <View style={[styles.wheel, styles.purpleWheel]}></View>
+      );
+    }
+    if(props.children.pressureValue < props.children.minPressureValue){
+      return (
+        <View style={[styles.wheel, styles.redWheel]}></View>
+      );
+    }
+    if(props.children.temperatureValue > props.children.maxTemperatureValue){
+      return (
+        <View style={[styles.wheel, styles.orangeWheel]}></View>
+      );
+    }
+    if(props.children.voltageValue < props.children.minVoltageValue){
+      return (
+        <View style={[styles.wheel, styles.yellowWheel]}></View>
+      );
+    } else {
+      return (
+        <View style={styles.wheel}></View>
+      );
+    }
+  }
+
   return (
-<View>
-{props.children.wheelId === 2 ? (
-  <View style={styles.wheelAxle}>
-    <View style={styles.wheel}></View>
-    <View style={styles.axle}></View>
-  </View>
-) : (
-  <View style={styles.wheel}></View>
-)}
-</View> 
+    <View>
+      {props.children.wheelId === 2 ? (
+        <View style={styles.wheelAxle}>
+          {renderWheelStyle()}
+          <View style={styles.axle}></View>
+        </View>
+      ) : (
+        <View>
+        {renderWheelStyle()}
+        </View>
+      )}
+    </View>
   );
 };
 
-
-
 const styles = StyleSheet.create({
-    wheelAxle: {
-        flexDirection: 'row',
-        alignItems: "center",
-        justifyContent: "center",
-
-      },
+  wheelAxle: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   wheel: {
     margin: 0.5,
     width: 25,
@@ -44,13 +70,32 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  purpleWheel: {
+    backgroundColor: colors.purpleWheel,
+    borderColor: colors.purpleWheelBorder,
+  },
+  redWheel: {
+    backgroundColor: colors.redWheel,
+    borderColor: colors.redWheelBorder,
+  },
+  orangeWheel: {
+    backgroundColor: colors.orangeWheel,
+    borderColor: colors.orangeWheelBorder,
+  },
+  yellowWheel: {
+    backgroundColor: colors.yellowWheel,
+    borderColor: colors.yellowWheelBorder,
+  },
+  noSignalWheel: {
+    backgroundColor: colors.noSignalBlue,
+  },
   axle: {
-    width: 45,  
-    height: 8.5,  
+    width: 45,
+    height: 8.5,
     backgroundColor: colors.axleBlue,
-    alignItems: 'center',
-    justifyContent: 'center',
-},
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 //height: 95,
 export default Wheel;
