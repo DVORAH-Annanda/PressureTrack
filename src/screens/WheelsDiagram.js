7721f3b503f00cbdee6ff66c6838c04a305de7ad
Annanda <Annanda@dvorahsolutions.co>
2022-01-24 08:56:23 +0200

    Refresh Sensor Values every minute

--- src/screens/WheelsDiagram.js
+++ src/screens/WheelsDiagram.js
@@ -11,33 +11,14 @@ import {
   StatusBar,
 } from "react-native";
 import { unitSensorValues } from "../actions/unitActions";
-import SensorValuesDiagram from "../screens/SensorValuesDiagram";
-//import { setLogout } from '../../redux/ducks/user'
+
+import AxleContainer from "../components/AxleContainer";
 
 import colors from "../styles/colors";
 
-import MetricsContainer from "../components/MetricsContainer";
-import TextBox from "../components/TextBox";
-import PressureBox from "../components/PressureBox";
-import NoSignalBox from "../components/NoSignalBox";
-import TemperatureBox from "../components/TemperatureBox";
-import AxleContainer from "../components/AxleContainer";
-//import LoadingBox from '../../components/LoadingBox'
-//import MessageBox from '../../components/MessageBox'
-
 const WheelsDiagram = ({ navigation, route }) => {
-  //const { route } = props;
   const { title, item } = route.params;
   const { id, nm } = item;
-
-  //const unitId = item.id
-  //console.log("UNIT ID: " + unitId)
-  //const dispatch = useReduxDispatch()
-  //const logoutHandler = () => dispatch(setLogout())
-  //const { unitId } = route.params.item.id
-
-  // const [unitId, setUnitId] = useState(0);
-  // const [axleSensorValues, setAxleSensorValues] = useState([]);
 
   const sensorValueProps = useSelector((state) => state.unitSensorValues);
   const { loading, error, sensorValues } = sensorValueProps;
@@ -53,6 +34,11 @@ const WheelsDiagram = ({ navigation, route }) => {
   useEffect(() => {
     console.log(`WheelsDiagram useEffect unit id ${id}`);
     dispatch(unitSensorValues(id));
+
+    const interval = setInterval(() => {
+      dispatch(unitSensorValues(id));
+    }, 60000);
+    return () => clearInterval(interval);
   }, [dispatch, unitSensorValues, id]);
 
   return (
